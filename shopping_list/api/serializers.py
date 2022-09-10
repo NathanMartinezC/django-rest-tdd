8f633e065a8a688548a679155a8454e23037e2ad
Nathan Martinez <n.mtz.cas@gmail.com>
2022-09-10 10:33:30 -0500

    Simple shopping list

---
+++ shopping_list/api/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from rest_framework import serializers
+
+from shopping_list.models import ShoppingItem
+
+
+class ShoppingItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingItem
+        fields = ["id", "name", "purchased"]
